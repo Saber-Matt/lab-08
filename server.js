8b77f30a27c3fa2545663479942b67a8e9d16fbb
Matthew Saber <mattsaber21@yahoo.com>
2021-06-29 07:57:53 -0700

    added s3 and other server functionality

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+
 import app from './lib/app.js';
 import pool from './lib/utils/pool.js';
+import { uploadFile, getFileStream } from './s3.js';
+import multer from 'multer';
 
 const PORT = process.env.PORT || 7890;
+
+const upload = multer({ dest: 'uploads/' })
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
@@ -13,7 +18,19 @@ process.on('exit', () => {
   pool.end();
 });
 
+app.get('/images/:key', (req, res) => {
+  const key = req.params.key
+  const readStream = getFileStream(key)
 
-app.post('/images', (req, res) => {
-  res.send("Excellent")
+  readStream.pipe(res)
+})
+
+app.post('/images', upload.single('image'), async (req, res) => {
+  const file = req.file
+  console.log(file)
+  const result = await uploadFile(file)
+  const description = req.body.description
+  res.send({ imagePath: `/images/${result.key}`)
 });
+
+app.listen(8080, () => console.log("Ello Gov'na, we're lis-ning on ort 8080"));
